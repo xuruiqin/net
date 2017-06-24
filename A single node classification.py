38b1af24f3b4bb21706e876005e59b9df1a9e32d
Lum Wang <2402997445@qq.com>
2017-06-24 12:18:41 +0800

    not sufficient

--- A single node classification.py
+++ A single node classification.py
@@ -2,14 +2,15 @@
 a cube can be seperated by a plane"""
 import numpy as np
 # The last element of each training data is minus one
-X= [[0,0,0,-1],[1,0,0,-1],[0,1,0,-1],[0,0,1,-1],[1,1,0,-1],[1,0,1,-1],[0,1,1,-1],[1,1,1,-1]]
+X= [[0,0,0,-1],[0,1,1,-1],[0,1,0,-1],[0,0,1,-1],[1,1,0,-1],[1,0,1,-1],[1,1,0,-1],[1,1,1,-1]]
 # Some points are labeled by -1, others are 1
-label=[-1,-1,-1,1,-1,1,1,1]
+label=[-1,1,-1,-1,-1,1,-1,1]
 # the weight and threshold value are set to random numbers between 1 and 1
 import random
 W=[]
 for i in range(4):
     W.append(random.uniform(-1,1))
+print("The original weight is:",W)
 # The activation function is double polarization step function
 def activation(x):
     if x>=0:
@@ -21,18 +22,22 @@ def activation(x):
 #    return 1/(1+np.exp(-x))
 # process of learning
 # total is the list of activation value
-total=[]
+total=0.0
 for i in range(8):
     for j in range(4):
-            total.append(X[i][j]*W[j])
-            delta=activation(total[i])
-            W[j]+=0.1*(label[i]-delta)*X[i][j]
+            total+=X[i][j]*W[j]
+    delta=activation(total)
+    for j in range(4):        
+            W[j]+=0.11*(label[i]-delta)*X[i][j]
+    print("Now",i,"weight is",W)
 # I hava get new weights after training, now let's test is. The outcome must comform to X and label
 test=[0]*4
 n=0.0
-for m in range(4):
-    test[m]=input("Please input the number,note:The last one is -1:")
+for m in range(3):
+    test[m]=input("Please input the coordinate:")
     test[m]=int(test[m])
+test[3]=-1
+for m in range(4):
     n+=test[m]*W[m]
 outcome=activation(n)
 print("My neural network's prediction of your input is:", outcome)
